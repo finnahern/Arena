7c7af9e28dedc761b7c3057f08de84d2b84b8470
finnahern <finna9x@gmail.com>
2020-06-06 13:19:39 +0100

    First commit, unfinished

---
+++ Character.java
@@ -0,0 +1,57 @@
+class Character{
+	
+	private int id;
+	private String name;
+	private int hp;
+	private int dmg;
+	
+	//Default Constructor
+	public Character(){
+		
+	}
+
+	//ID Getter/Setter
+	public int getID(){
+		
+		return id;
+	}
+
+	public void setID(int id){
+		
+		this.id = id;
+	}
+
+	//Name Getter/Setter
+	public String getName(){
+		
+		return name;
+	}
+
+	public void setName(String name){
+		
+		this.name = name;
+	}
+	
+	//HP Getter/Setter
+	public int getHP(){
+		
+		return hp;
+	}
+	
+	public void setHP(int hp){
+		
+		this.hp = hp;
+	}	
+	
+	//DMG Getter/Setter
+	public int getDMG(){
+	
+		return dmg;
+	}
+	
+	public void setDMG(int dmg){
+		
+		this.dmg = dmg;
+	}
+}	
+	
